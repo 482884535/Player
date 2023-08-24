77bc8b0ff98b56e047407e182b51edfb8cb46782
482884535 <76425856+482884535@users.noreply.github.com>
2023-08-25 00:06:48 +0800

    2023－8/25

--- index.php
+++ index.php
@@ -1,9 +1,70 @@
+<?php
+$url = $_GET['url'];
+if (empty($url)) {
+exit('<html>
+<head>
+  <meta charset="UTF-8" />
+  <title>残月解析 - 弹幕播放器</title>
+  <link rel="shortcut icon" href="favicon.ico">
+  <meta name="keywords" content="残月解析,弹幕播放器">
+  <meta name="description" content="残月解析 - 弹幕播放器 ">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+  <meta name="x5-orientation" content="portrait" />
+  <meta name="renderer" content="webkit" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <style>
+body,html {
+	font: 18px"Microsoft Jhenghei",Arial,Lucida Grande,Tahoma,sans-serif;
+	width: 100%;
+	height: 100%;
+	padding: 0;
+	margin: 0;
+	overflow-x: hidden;
+	overflow-y: hidden;
+	background-color: black
+}
+
+#loading {
+	width: 100%;
+	height: 100%;
+	padding: 0;
+	margin: 0;
+	text-align: center;
+	display: table;
+	position: absolute;
+	z-index: 10000000001;
+	background-size: 100%100%
+}
+
+h1 {
+	color: #ffffff;
+	font-size: 1.2rem;
+	margin: 0;
+	padding: 0;
+	vertical-align: middle;
+	display: table-cell
+}
+</style>
+</script>
+ </head>
+ <body>
+  </div>
+  <div id="loading" class="center">
+<h1 style="text-shadow: 2px 2px 5px #d5ebe1;">仅供内部交流研究，请勿用于任何商业用途 <br><br><br><br><br>    <font size="10">解析失败 URL为空，请稍后再试 ~ </font><br><br><font size="3"><p></p><p><p><br></p><p><br></font><font size="2"><br>本站均来于互联网，请勿用于商业用途，仅供学习交流
+<br><a href="/" target="_Blank"> Copyright © 2023 All Rights Reserved </a></font></h1>
+</body>
+</html>');
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 <meta charset="UTF-8">
 <title> 残月解析 - 弹幕播放器</title>
+<link rel="shortcut icon" href="favicon.ico">
+<meta name="keywords" content="残月解析,弹幕播放器">
+<meta name="description" content="残月解析 - 弹幕播放器 ">
 <link rel="stylesheet" href="./css/canyue.css">
 <style> 
 .qianyi-full-icon span svg,.qianyi-fulloff-icon span svg{display: none;}
@@ -38,8 +99,8 @@ css+='</style>';
 <script>
     // 播放器基本设置
     var playlink ="",urlpar ='canyue';
-    var dmapi = 'https://api.noome.cn/dmku',vodurl = '<?php echo $_GET['url']; ?>',vodid="",vodsid="",vodpic="https://api.mysqil.com/API/bing.php",vodname="",next = "";
-    var pic="https://api.mysqil.com/API/bing.php";
+    var dmapi = 'https://api.noome.cn/dmku',vodurl = '<?php echo $_GET['url']; ?>',vodid="",vodsid="",vodpic="https://api.dujin.org/bing/1920.php",vodname="",next = "";
+    var pic="https://api.dujin.org/bing/1920.php";
     var playnext = ""+next ;
     var user = '',group = "",color = '#00a1d6',logo ='',autoplay = false;
     var danmuon = 1,laoding = 1,diyvodid = 0,pause_ad = 0,usernum = "<?php echo mt_rand(100,10000); ?>";;
